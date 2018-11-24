72682fa8baed630bfcfc7e8ce699a3515d79efd4
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2018-11-24 20:43:56 +0300

    +ReactiveTarget, +Shoot, +WanderingAI

---
+++ FPShooter/Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour {
+
+    public float speed = 3.0f;//значение скорости движения и расстояния
+
+    public float obstacleRange = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.Translate(0, 0, speed * Time.deltaTime);//непрерывно движемся вперед в каждом кадре
+    }
+
+}
