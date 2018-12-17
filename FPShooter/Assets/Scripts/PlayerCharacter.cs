2c5535178fe7c0672a86bf4f5d01ade48c90c61a
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2018-12-17 20:50:19 +0300

    +CollectibleItem +DeviceTrigger

--- FPShooter/Assets/Scripts/PlayerCharacter.cs
+++ FPShooter/Assets/Scripts/PlayerCharacter.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour {
+public class PlayerCharacter : MonoBehaviour
+{
 
     private int _health;
-   
+    private int maxHealth;
+
+
     void Start()
     {
-        _health = 5;//инициализация переменной
+        _health = 50;//инициализация переменной
+        maxHealth = 100; //инициализация максимального здоровья
     }
 
     public void Hurt(int damage)
@@ -16,4 +20,19 @@ public class PlayerCharacter : MonoBehaviour {
         _health -= damage;//уменьшение здоровья игрока
         Debug.Log("Health: " + _health);
     }
+
+    public void ChangeHealth(int value)
+    { // Дргие сценарии не могут напрямую задавать переменную health,но могут вызвать эту функцию
+        _health += value;
+        if (_health > maxHealth)
+        {
+            _health = maxHealth;//не даем жизни подняться выше максимума
+        }
+        else if (_health < 0)
+        {
+            _health = 0;//не даем уменьшать жизнь в минус
+        }
+        Debug.Log("health: " + _health + "/" + maxHealth);
+    }
 }
+
