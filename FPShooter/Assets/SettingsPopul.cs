5556c0706b965a0af72e765b2e9e5792784fc7e4
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2018-12-22 22:02:54 +0300

    +GUI +Animation

---
+++ FPShooter/Assets/SettingsPopul.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPopul : MonoBehaviour {
+
+
+    public void Open()
+    {
+        gameObject.SetActive(true);//активируем этот объект, чтобы открыть окно
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);//диактивируем объект, чтобы закрыть окно
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
