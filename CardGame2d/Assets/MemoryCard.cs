fb881ea92ccb462017bd5d829c8aedad9cf079a7
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2019-01-05 14:41:04 +0300

    + fix CardGame2d

---
+++ CardGame2d/Assets/MemoryCard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+
+
+    public GameObject cardBack; //переменная появиться на панели Inspector
+    public SceneController controller;
+
+    private int _id;
+
+    public int id
+    {
+        get { return _id; } // добавление функции чтения
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)//запускаем код деактивации
+        {
+            cardBack.SetActive(false); //Делаем объект видимым/невидимым
+        }
+    }
+
+    public void SetCard(int id, Sprite image)//метод для передачи указанному объекту новых спрайтов
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ CardGame2d/Assets/SceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+    [SerializeField] private MemoryCard originalCard;//ссылка для карты в сцене
+    [SerializeField] private Sprite[] images;//массив ссылок на ресурсы-спрайты
+
+    void Start()
+    {
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);//вызов открытого метода в MemoryCard
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
